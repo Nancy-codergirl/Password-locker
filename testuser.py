99184188d7efe9e1189ccf8d512db3cb66ef5540
Nancy-codergirl <nancymuriithi@student.moringaschool.com>
2021-10-26 17:52:22 +0300

    Add test class to confirm login

---
+++ testuser.py
@@ -0,0 +1,15 @@
+import unittest
+from user import  User
+
+class UserTestCreate(unittest.TestCase):
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def setUp(self):
+        '''
+        This method is responsible for setting up an intialy state before each test begin
+        '''
+        self.user = User("Nancy", "12345", "n@mail.com")        
