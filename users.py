69773b8685e0afe542bf3dc8611149ee025a0a88
Nancy-codergirl <nancymuriithi@student.moringaschool.com>
2021-10-26 17:06:39 +0300

    Add method to create instance of user

--- users.py
+++ users.py
@@ -1,2 +1,18 @@
-class Users:
-    pass
+class User:
+    '''
+    Class that creates a new instance of a user
+    '''
+    user_list = [] #store the users
+    def __init__(self, username, password, email):
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            username: New user username.
+            password : New user password.
+            email : New user email.
+        '''
+
+        self.username = username
+        self.password = password
+        self.email = email
