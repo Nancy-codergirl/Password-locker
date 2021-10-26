21d814035dd188f40e2495fda15651cfb82c7ea7
Nancy-codergirl <nancymuriithi@student.moringaschool.com>
2021-10-26 17:54:44 +0300

    Add deproperty to test the code if working correctly

--- testuser.py
+++ testuser.py
@@ -13,3 +13,23 @@ class UserTestCreate(unittest.TestCase):
         This method is responsible for setting up an intialy state before each test begin
         '''
         self.user = User("Nancy", "12345", "n@mail.com")        
+
+ def setUp(self):
+        '''
+        This method is responsible for setting up an intialy state before each test begin
+        '''
+        self.user = User("Nancy", "12345", "n@mail.com")        
+
+    def tearDown(self):
+        '''
+        This method is responsible for resetting the test env back to intial state by destroying the listed properties
+        '''
+        User.user_list = [] 
+
+         def test_init(self):
+        '''
+        A test to check if the class is beign intialized correctly
+        '''
+        self.assertEqual(self.user.username,"Nancy")
+        self.assertEqual(self.user.password,"12345")
+        self.assertEqual(self.user.email,"n@mail.com")
