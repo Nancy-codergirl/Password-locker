e061469d245e5f21afe157ac4014d2bfb858d1ba
Nancy-codergirl <nancymuriithi@student.moringaschool.com>
2021-10-26 17:17:43 +0300

    Add new user and init

--- credential.py
+++ credential.py
@@ -1,2 +1,22 @@
+import pyperclip
+
+
 class Credential:
-    pass
+    """
+    a class that creates new credentials 
+    """
+    credential_list = []
+
+    def __init__(self, account_name,username, password):
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            credential_name: New credential credential_name.
+            password : New credential password.
+            number: New contact phone number.
+        '''
+        self.account_name = account_name        
+        self.username =username
+        self.password = password
+
